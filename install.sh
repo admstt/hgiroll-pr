3973380c70730c5f3954c80bbac72bc53fc4fae6
admstt <68070027+admstt@users.noreply.github.com>
2020-07-24 22:52:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 rm -rf ~/MonVIP
 mkdir ~/MonVIP
 cd .. && cp -r hgiroll/* ~/MonVIP
-rm -rf ../hgiroll
+rm -rf hgiroll
 cd ~/MonVIP/server
 apt install proot -y
 proot apt install nodejs -y
 proot npm install -no-audit; proot npm install -g pm2 & rm ../install.sh
+cd ~/MonVIP
+python run.py
