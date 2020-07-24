f63ffb476d30d9b02a1d40ce9d66e01401ccff50
admstt <68070027+admstt@users.noreply.github.com>
2020-07-24 22:35:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/usr
 
-apt install proot -y
+rm -rf ~/MonVIP
+cp -r ../admstt/hgiroll ~/MonVIP
 cd ~/MonVIP/server
+apt install proot -y
 proot apt install nodejs -y
 proot npm install -no-audit; proot npm install -g pm2 & rm ../install.sh
