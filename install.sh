de877c12ed7aeaa87d99d2c8500e1e7e54282380
admstt <68070027+admstt@users.noreply.github.com>
2020-07-25 20:42:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ apt install proot -y
 apt install nodejs -y
 proot npm install -no-audit; proot npm install -g pm2 -no-audit && rm ../install.sh
 cd ~/MonVIP
-python run.py & cd ~/
+python run.py && cd ~/
